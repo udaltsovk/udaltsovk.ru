104960c0b42af0b2e1355c10917959760cfe5c35
Kirill Udaltsov <commits@udaltsovk.ru>
2026-05-04 11:51:47 +0300

    chore: migrate over to bun

--- shell.nix
+++ shell.nix
@@ -1,18 +1,11 @@
 {pkgs ? import <nixpkgs> {}}: let
   packages = with pkgs; [
-    # languages
-    (nodejs_24.override {
-      enableNpm = false;
-    })
-    pnpm
-
-    # development tools
-    watchexec
+    bun
   ];
 in
   with pkgs;
     mkShell {
-      name = "edu-frontend";
+      name = "udaltsovk.ru";
       buildInputs = packages;
 
       DIRENV_LOG_FORMAT = "";
